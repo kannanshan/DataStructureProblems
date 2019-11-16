4eead31e39fcbdf3866bbf064cb80cc3de1a0b23
Kannan <kannan@freshdesk.com>
2019-11-16 12:29:11 +0530

    Problems

---
+++ HackerRankProblems/src/com/hackerrank/problems/Solutions.java
@@ -0,0 +1,32 @@
+package com.hackerrank.problems;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solutions {
+
+
+	private static int get(int row, int i, int[][] container) {
+		int sum = 0;
+		for (int j = 0; j < row; j++) {
+			if (i != j) {
+				sum = sum + container[i][j];
+			}
+		}
+		return sum;
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		int[][] container ={{0 ,2 ,1},
+				{1, 1, 1},
+				{2 ,0 ,0}};
+		//System.out.println(organizingContainers(container));
+	}
+}
