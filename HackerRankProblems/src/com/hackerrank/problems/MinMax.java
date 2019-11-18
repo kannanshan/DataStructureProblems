792b2f6aadd6de1fff979813c763f5d7b8eaa1f2
Kannan <kannan@freshdesk.com>
2019-11-18 11:01:30 +0530

    Array problem

---
+++ HackerRankProblems/src/com/hackerrank/problems/MinMax.java
@@ -0,0 +1,16 @@
+package com.hackerrank.problems;
+
+import java.util.Arrays;
+
+public class MinMax {
+
+	static void miniMaxSum(int[] arr) {
+		int val = 0;
+		for (int in : arr) {
+			val = val + in;
+		}
+		Arrays.sort(arr);
+		System.out.println((val - arr[arr.length]) + " " + (val - arr[0]));
+	}
+
+}
--- HackerRankProblems/src/com/hackerrank/problems/Sum.java
+++ HackerRankProblems/src/com/hackerrank/problems/Sum.java
@@ -1,15 +1,34 @@
 package com.hackerrank.problems;
 
-public class Sum {
-	
-	static long aVeryBigSum(long[] ar) {
-		long ans = 0;
-		for(long l : ar)
-		{
-			ans=ans+l;
-		}
-		return ans;
+import java.text.DecimalFormat;
 
-    }
+public class Sum {
+
+	static void plusMinus(int[] arr) {
+		double pos = 0;
+		double neg = 0;
+		double zero = 0;
+		for (int in : arr) {
+			if (in > 0)
+				pos++;
+			if (in < 0)
+				neg++;
+			if (in == 0)
+				zero++;
+		}
+		DecimalFormat numberFormat = new DecimalFormat("#.000000");
+		double val = pos / new Double(arr.length);
+		System.out.println(numberFormat.format(val));
+		val = neg / new Double(arr.length);
+		System.out.println(numberFormat.format(val));
+		val = zero / new Double(arr.length);
+		System.out.println(numberFormat.format(val));
+
+	}
+
+	public static void main(String args[]) {
+		int[] arr = { -4, 3, -9, 0, 4, 1 };
+		plusMinus(arr);
+	}
 
 }
