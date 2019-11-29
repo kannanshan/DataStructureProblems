b7acb570b266225d5d6d4877fbbd9e24a71b7dd1
Kannan <kannan@freshdesk.com>
2019-11-29 16:36:32 +0530

    Researchcitation

--- HackerRankProblems/src/com/hackerrank/easy/problems/ReserachCitation.java
+++ HackerRankProblems/src/com/hackerrank/easy/problems/ReserachCitation.java
@@ -5,9 +5,17 @@ import java.util.Arrays;
 public class ReserachCitation {
 
 	public static int hIndex(int[] citations) {
+		
+		char ch = 23422;
 		int result = 0;
 		Arrays.sort(citations);
-
+		if(citations.length==1)
+		{
+			if(citations[0]!=0)
+				return citations[0];
+			else
+				return 0;
+		}
 		for (int i = citations.length - 1; i >= 0; i--) {
 			result++;
 			if (result >= citations[i])
