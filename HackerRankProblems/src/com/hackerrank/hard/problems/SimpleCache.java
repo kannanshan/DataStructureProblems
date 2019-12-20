5bec4f2b03dcb802f7f2bf6f958a82765708eadf
Kannan <kannan@freshdesk.com>
2019-12-20 12:04:42 +0530

    Cache

---
+++ HackerRankProblems/src/com/hackerrank/hard/problems/SimpleCache.java
@@ -0,0 +1,9 @@
+package com.hackerrank.hard.problems;
+
+public class SimpleCache {
+	
+	public void createBucket(String bucketName);
+	public void putValue(String bucketName,String key,byte[] value);
+	public byte[] getValue(String bucketName,String key);
+
+}
